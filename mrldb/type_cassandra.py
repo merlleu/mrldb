5d6765992adcb4a8a7df09cd73527e82b30821b2
merlleu <r.langdorph@gmail.com>
2019-05-01 00:37:32 +0200

    0.0.12 - fix cassandra

--- mrldb/type_cassandra.py
+++ mrldb/type_cassandra.py
@@ -9,7 +9,7 @@ class MrlDBCassandra:
         from cassandra.cluster import Cluster
         if isinstance(cluster, str):
             cluster=[cluster]
-        if (user==None or password==None):
+        if (username==None or password==None):
             self.cluster=Cluster(cluster)
         else:
             from cassandra.auth import PlainTextAuthProvider
@@ -17,35 +17,34 @@ class MrlDBCassandra:
             self.cluster=Cluster(cluster, auth_provider=self.auth_provider)
         if db!=None:
             self.db=self.cluster.connect(db)
-            self.connection=self.db
+            self.cursor=self.db
         else:
-            self.connection=self.cluster
+            self.cursor=self.cluster
         self.structure=structure
-        self.cursor=self.db
-        self._config={"system":"cassandra", "cluster": cluster,"database": name, "structure": f"{len(structure) if structure!=None else '0'} tables", "username": username, "password":password}
+        self._config={"system":"cassandra", "cluster": cluster,"database": db, "structure": f"{len(structure) if structure!=None else '0'} tables", "username": username, "password":password}
         return
     def insert(self, table, data):
         def frmt(d):
             return f"'{d}'" if isinstance(d, str) else str(d)
-        return self.connection.execute(f"INSERT INTO {table} ({', '.join([frmt(x) for x in data.keys()])}) VALUES ({', '.join([frmt(x) for x in data.values()])})")
+        return self.cursor.execute(f"INSERT INTO {table} ({', '.join([frmt(x) for x in data.keys()])}) VALUES ({', '.join([frmt(x) for x in data.values()])})")
     def update(self, table, data, conds=None):
         def frmt(d):
             return f"'{d}'" if isinstance(d, str) else str(d)
-        return self.connection.execute(f"UPDATE {table} SET {', '.join([key+'='+frmt(arg) for key, arg in data.items()])}{' WHERE '+conds if conds!=None else ''}")
+        return self.cursor.execute(f"UPDATE {table} SET {', '.join([key+'='+frmt(arg) for key, arg in data.items()])}{' WHERE '+conds if conds!=None else ''}")
     def select(self, table, columns, conds=None):
         if self.structure!=None:
             if columns=="*":columns=self.structure[table].keys()
             return [
             {_col:_var for _col, _var in zip(columns, record)}
             for record in
-            self.connection.execute(f"SELECT {'*' if columns=='*' else ', '.join(columns)} FROM {table}{' WHERE '+conds if conds!=None else ''}")
+            self.cursor.execute(f"SELECT {'*' if columns=='*' else ', '.join(columns)} FROM {table}{' WHERE '+conds if conds!=None else ''}")
             ]
         else:
-            return self.connection.execute(f"SELECT {'*' if columns=='*' else ', '.join(columns)} FROM {table}{' WHERE '+conds if conds!=None else ''}")
+            return self.cursor.execute(f"SELECT {'*' if columns=='*' else ', '.join(columns)} FROM {table}{' WHERE '+conds if conds!=None else ''}")
     def _getinfos(self):
         return self._config
     def init(self):
-        [self.connection.execute(command) for command in get_struct_init(self.structure)]
+        [self.cursor.execute(command) for command in get_struct_init(self.structure)]
         return self
     def __str__(self):
         return f"<mrldb.MrlDBCassandra at {id(self)} - connection: {self._config['cluster']}>"
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 ##version#start##
-__version__='0.0.10'
+__version__='0.0.12'
 ##version#end##
 setuptools.setup(
     name="mrldb",
