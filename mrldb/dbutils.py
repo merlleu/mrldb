6bf14b31761e0dbf0db327299b91a97050af40af
merlleu <r.langdorph@gmail.com>
2019-05-04 13:37:51 +0200

    v0.0.20 fix NoneType to NULL conversion

--- mrldb/dbutils.py
+++ mrldb/dbutils.py
@@ -9,14 +9,14 @@ class _dbutils:
         # table= "mytable"
         # data= {"name": "tom", "age": 1}
         def frmt(d):
-            return f"'{d}'" if isinstance(d, str) else str(d)
+            return f"'{d}'" if isinstance(d, str) else ("null" if d==None else str(d))
         return f"INSERT INTO {table}({', '.join([x for x in data.keys()])}) VALUES ({', '.join([frmt(x) for x in data.values()])})"
     def update(self, table, data, conds=None):
         # table= "mytable"
         # data= {"name": "tom", "age": 1}
         # conds= "(x=1) or (x=2 and y=3)"
         def frmt(d):
-            return f"'{d}'" if isinstance(d, str) else str(d)
+            return f"'{d}'" if isinstance(d, str) else ("null" if d==None else str(d))
         return f"UPDATE {table} SET {', '.join([key+'='+frmt(arg) for key, arg in data.items()])}{' WHERE '+conds if conds!=None else ''}"
     def select(self, table, columns, conds=None):
         # table= "mytable"
@@ -24,21 +24,3 @@ class _dbutils:
         # conds= "(x=1) or (x=2 and y=3)"
         return f"SELECT {'*' if columns=='*' else ', '.join(columns)} FROM {table}{' WHERE '+conds if conds!=None else ''}"
 dbutils=_dbutils()
-def insert(table, data):
-    # table= "mytable"
-    # data= {"name": "tom", "age": 1}
-    def frmt(d):
-        return f"'{d}'" if isinstance(d, str) else str(d)
-    return f"INSERT INTO {table}({', '.join([x for x in data.keys()])}) VALUES ({', '.join([frmt(x) for x in data.values()])})"
-def update(table, data, conds=None):
-    # table= "mytable"
-    # data= {"name": "tom", "age": 1}
-    # conds= "(x=1) or (x=2 and y=3)"
-    def frmt(d):
-        return f"'{d}'" if isinstance(d, str) else str(d)
-    return f"UPDATE {table} SET {', '.join([key+'='+frmt(arg) for key, arg in data.items()])}{' WHERE '+conds if conds!=None else ''}"
-def select(table, columns, conds=None):
-    # table= "mytable"
-    # columns= ["test", "x"]
-    # conds= "(x=1) or (x=2 and y=3)"
-    return f"SELECT {'*' if columns=='*' else ', '.join(columns)} FROM {table}{' WHERE '+conds if conds!=None else ''}"
