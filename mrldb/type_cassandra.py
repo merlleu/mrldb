44fb201ea8a4eb790271927b64165ebd8540746a
merlleu <mrerl@warlegend.net>
2023-10-15 01:36:08 +0200

    Update type_cassandra.py

--- mrldb/type_cassandra.py
+++ mrldb/type_cassandra.py
@@ -10,17 +10,16 @@ class MrlDBCassandra:
         if isinstance(cluster, str):
             cluster=[cluster]
         if (username==None or password==None):
-            self.cluster_=Cluster(cluster)
+            self.cluster=Cluster(cluster)
         else:
             from cassandra.auth import PlainTextAuthProvider
             self.auth_provider = PlainTextAuthProvider(username=username, password=password)
-            self.cluster_=Cluster(cluster, auth_provider=self.auth_provider)
-        if db is not None:
-            self.db=self.cluster_.connect(db)
+            self.cluster=Cluster(cluster, auth_provider=self.auth_provider)
+        if db!=None:
+            self.db=self.cluster.connect(db)
             self.cursor=self.db
         else:
-            self.cluster_=self.cluster
-            self.cursor=self.cluster_.connect()
+            self.cursor=self.cluster
         self.structure=structure
         self._config={"system":"cassandra", "cluster": cluster,"database": db, "structure": f"{len(structure) if structure!=None else '0'} tables", "username": username, "password":password}
         return
